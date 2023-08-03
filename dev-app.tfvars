f38e2cbb54761d73f7a906dfdbebe55ddf1e6126
Mark McLeod <mark.mcleod@digital.homeoffice.gov.uk>
2023-08-03 20:02:40 +0100

    Initial Commit

--- dev-app.tfvars
+++
@@ -1,8 +0,0 @@
-# Common
-project = "sas-codepipeline"
-
-# General 
-aws_region = "eu-west-2"
-
-ami="ami-08d4ac5b634553e16"
-availability_zone ="eu-west-2a"
--- main.tf
+++ main.tf
@@ -1,24 +1,3 @@
-# terraform apply -var-file="app.tfvars" -var="createdBy=e2esa"
-
-locals {
-  tags = {
-    Project     = var.project
-    CreatedBy   = var.createdBy
-    CreatedOn   = timestamp()
-    Environment = terraform.workspace
-  }
+output "hello_world" {
+  value = "Hello, World!"
 }
-
-module "ec2-instance_example_complete" {
-  source  = "terraform-aws-modules/ec2-instance/aws//examples/complete"
-  version = "5.2.1"
-}
-
-#module "ec2" {
-  #source            = "../../modules/e2esa-module-aws-ec2"
-#  source             = "git::https://github.com/e2eSolutionArchitect/terraform.git//providers/aws/modules/e2esa-module-aws-ec2?ref=main"
-  #for_each          = toset(var.ec2_names) # toset(["ec21","ec22"])
-#  ami               = var.ami
-#  availability_zone = var.availability_zone
-#  tags              = local.tags
-#}
